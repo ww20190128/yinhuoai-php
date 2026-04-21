92e0ca07c9c9fde3f725d7d5d6be8fbf4950f5f2
“ww20190128” <ww20190128@gmail.com>
2026-04-21 22:42:11 +0800

    x

--- projects/yinhuo/ctrl/ShareKs.php
+++ projects/yinhuo/ctrl/ShareKs.php
@@ -23,9 +23,56 @@ class ShareKs extends CtrlBase
     	if (empty($code)) {
     		throw new $this->exception('请求参数错误');
     	}
-    	
     	$shareKs = \service\ShareKs::singleton();
-    	$shareKs->code2AccessToken($code);
+    	return $shareKs->code2AccessToken($code);
+    }
+    
+    /**
+     * 上传视频
+     *
+     * @return array
+     */
+    public function upload()
+    {
+    	$params = $this->params;
+    	if (empty($this->userId)) {
+    		throw new $this->exception('登录已过期，请重新登录', array('status' => 2));
+    	}
+    	$clipId = $this->paramFilter('id', 'string');
+    	if (empty($clipId)) {
+    		throw new $this->exception('请求参数错误');
+    	}
+    	$upload_token = $this->paramFilter('upload_token', 'string');
+    	$endpoint = $this->paramFilter('endpoint', 'string');
+    	if (empty($upload_token) || empty($endpoint)) {
+    		throw new $this->exception('请求参数错误');
+    	}
+    	$shareKs = \service\ShareKs::singleton();
+    	return $shareKs->upload($clipId, $upload_token, $endpoint);
     }
 
+    /**
+     * 发布视频
+     * 
+     * @return array
+     */
+    public function publish()
+    {
+    	$params = $this->params;
+    	if (empty($this->userId)) {
+    		throw new $this->exception('登录已过期，请重新登录', array('status' => 2));
+    	}
+    	$clipId = $this->paramFilter('id', 'string');
+    	if (empty($clipId)) {
+    		throw new $this->exception('请求参数错误');
+    	}
+    	$access_token = $this->paramFilter('access_token', 'string');
+    	$upload_token = $this->paramFilter('upload_token', 'string');
+    	if (empty($access_token) || empty($upload_token)) {
+    		throw new $this->exception('请求参数错误');
+    	}
+    	$shareKs = \service\ShareKs::singleton();
+    	return $shareKs->publish($clipId, $access_token, $upload_token);
+    }
+    
 }
--- projects/yinhuo/service/ShareKs.php
+++ projects/yinhuo/service/ShareKs.php
@@ -15,6 +15,12 @@ class ShareKs extends ServiceBase
      */
     private static $instance;
 
+    // 配置
+    private static $conf = array(
+    	'app_id' => 'ks654147848518544440',
+    	'app_secret' => '5uIis5sYxWx1dTDT_7zhNw',
+    );
+    
     /**
      * 单例模式
      *
@@ -35,16 +41,75 @@ class ShareKs extends ServiceBase
      */
     public function code2AccessToken($code)
     {
-    	$url = "https://open.kuaishou.com";
-    	$data = array(
-    		'app_id' => 'ks654147848518544440',
-    		'app_secret' => '5uIis5sYxWx1dTDT_7zhNw',
-    		'code' => $code,
-    		'grant_type' => 'authorization_code',
-    	);
-    	$url = "https://open.kuaishou.com/openapi/user_info?appid={$data['app_id']}&app_secret={$data['app_secret']}&code={$code}&grant_type=authorization_code";
+    	$conf = self::$conf;
+    	$url = "https://open.kuaishou.com/openapi/user_info?appid={$conf['app_id']}&app_secret={$conf['app_secret']}&code={$code}&grant_type=authorization_code";
     	$response = httpGetContents($url);
-    	return $response;
+    	$response = empty($response) ? array() : json_decode($response, true);
+    	$access_token = empty($response['access_token']) ? '' : $response['access_token']; // 认证信息
+    	if (empty($access_token)) {
+    		throw new $this->exception('请重新授权');
+    	}
+    	$uploadInfo = $this->startUpload($access_token);
+    	$uploadInfo['access_token'] = $access_token;
+    	return $uploadInfo;
     }
 
+    /**
+     * 发起上传
+     * 
+     * @return array
+     */
+    private function startUpload($accessToken)
+    {
+    	$conf = self::$conf;
+    	$url = "https://open.kuaishou.com/openapi/photo/start_upload";
+    	$conf = self::$conf;
+    	$data = array(
+    		'access_token' => $access_token,
+    		'app_id' => $conf['app_id'],
+    	);
+    	$response = doPost($url, $data);
+    	$response = empty($response) ? array() : json_decode($response, true);
+    	if (empty($response['upload_token']) || empty($response['endpoint'])) {
+    		throw new $this->exception('请重新授权');
+    	}
+    	return $response;
+    }
+    
+    /**
+     * 上传
+     *
+     * @return array
+     */
+    public function upload($clipId, $uploadToken, $endpoint)
+    {
+    	$conf = self::$conf;
+    	$url = "http://{$endpoint}/api/upload";
+    	$conf = self::$conf;
+    	$data = array(
+    		'upload_token' => $uploadToken,
+    	);
+    	$response = doPost($url, $data);
+    	$response = empty($response) ? array() : json_decode($response, true);
+    	return $response;
+    }
+    
+    /**
+     * 发布
+     *
+     * @return array
+     */
+    public function publish($clipId, $uploadToken, $endpoint)
+    {
+    	$conf = self::$conf;
+    	$url = "https://open.kuaishou.com/openapi/photo/publish";
+    	$conf = self::$conf;
+    	$data = array(
+    		'cover' => $cover, // 封面
+    		'caption' => $caption, // 标题
+    	);
+    	$response = doPost($url, $data);
+    	$response = empty($response) ? array() : json_decode($response, true);
+    	return $response;
+    }
 }
