311427a83f564fab4af49b6cf7de976fd31ccae9
“ww20190128” <ww20190128@gmail.com>
2026-04-28 12:53:49 +0800

    x

--- projects/yinhuo/service/Pay.php
+++ projects/yinhuo/service/Pay.php
@@ -249,6 +249,8 @@ class Pay extends ServiceBase
 			$response = self::$weChatPayInstance->chain('v3/pay/transactions/jsapi')->post(array('json' => $data));
 			$response = empty($response) ? '' : $response->getBody()->getContents();
 		} catch (\Exception $e) {
+			
+		print_r($e);
 			return false;
 		}
     	
