774a95054ade49f1cd98029ccb6d329c22a3b45d
“ww20190128” <ww20190128@gmail.com>
2026-05-10 21:08:46 +0800

    x

--- projects/yinhuo/service/Pay.php
+++ projects/yinhuo/service/Pay.php
@@ -358,13 +358,14 @@ class Pay extends ServiceBase
 			$response = self::$weChatPayInstance->chain('v3/pay/profitsharing/orders')->post(array('json' => $data));
 			$response = empty($response) ? '' : $response->getBody()->getContents();
 		} catch (\Exception $e) {
-			
-			print_r($e);exit;
-			$file = CACHE_PATH . 'payNotify1.txt';
+
+			$file = CACHE_PATH . 'profitsharing.txt';
 			@file_put_contents($file, json_encode($e));
 			return false;
 		}
-		print_r($response);exit;
+
+		$file = CACHE_PATH . 'profitsharing2.txt';
+		@file_put_contents($file, json_encode($response));
 		return true;
 	}
 }
