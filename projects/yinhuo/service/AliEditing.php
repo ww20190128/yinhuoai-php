c93207a40493d61e9b638433e2786e1a717b6e53
“ww20190128” <ww20190128@gmail.com>
2026-06-17 15:18:38 +0800

    x

--- projects/yinhuo/service/AliEditing.php
+++ projects/yinhuo/service/AliEditing.php
@@ -1033,7 +1033,6 @@ $lensRow['transitionIds'] = array('fade');
 	public function submitMediaProducingJob($chipParam)
 	{
 		$timeline = self::getTimeline($chipParam);
-	print_r($timeline);exit;	
 
 		$orientation = 'Horizontal';
 		$width = $height = 0;
